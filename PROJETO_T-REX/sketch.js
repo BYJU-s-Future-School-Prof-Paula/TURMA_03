bca17812c2a199aa8d2b485fdb573efd16dac7c6
Paula Gibrim <49460223+paulagibrim@users.noreply.github.com>
2022-06-01 20:50:18 -0300

    Add files via upload

--- PROJETO_T-REX/sketch.js
+++ PROJETO_T-REX/sketch.js
@@ -1,5 +1,6 @@
 var trex, trexCorrendo;
-var chao, chaoImagem;
+var chao, chaoImagem, chaoInvisivel;
+
 
 // Funçao para carregar os arquivos que vao ser usados
 function preload() {
@@ -17,8 +18,15 @@ function setup(){
   trex.addAnimation("trex quando está correndo", trexCorrendo);
   trex.scale = 0.5;
 
-  chao = createSprite(300,190,600,10);
+  // cria e configura o chao
+  chao = createSprite(300,175,600,10);
   chao.addImage("chao do jogo", chaoImagem);
+  chao.velocityX = -3;
+
+  chaoInvisivel = createSprite(300,190,600,10);
+  chaoInvisivel.visible = false;
+
+
 }
 
 function draw(){
@@ -26,17 +34,26 @@ function draw(){
   drawSprites();
 
   pular();
+  infinity();
 
   // trex colide com o chao (nao cai)
-  trex.collide(chao);
+  trex.collide(chaoInvisivel);
+
+  console.log(chao.x);
 }
 
 // funcao que permite o dino a pular
 function pular() {
     //keyIsDown(UP_ARROW)
-  if(keyDown("space")){
+  if(keyDown("space") && trex.isTouching(chao) ){
     trex.velocityY = -13;
   }
   trex.velocityY += 0.8;  
 }
 
+// funcao que permite o chao ser ibfinito (nao acabar nunca)
+function infinity() {
+  if(chao.x < 0){
+    chao.x = chao.width/2;
+  }
+}
