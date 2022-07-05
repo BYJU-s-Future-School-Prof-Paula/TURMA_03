8a6b0b859b8d6ada815dc59995ba94a63ac73947
Paula Gibrim <49460223+paulagibrim@users.noreply.github.com>
2022-07-04 21:10:47 -0300

    Add files via upload

--- PROJETO_T-REX/sketch.js
+++ PROJETO_T-REX/sketch.js
@@ -1,17 +1,19 @@
-var trex, trexCorrendo, trexMorto;
-var chao, chaoImagem, chaoInvisivel;
-var nuvemImg;
-var numAleat;
-var cactoimg1, cactoimg2, cactoimg3;
-var cactoimg4, cactoimg5, cactoimg6;
-var pontos = 0;
-var gameOver, restart;
-var gameOverImg, restartImg;
-var grupocactos, gruponuvens;
+let trex, trexCorrendo, trexMorto;
+let chao, chaoImagem, chaoInvisivel;
+let nuvemImg;
+let numAleat;
+let cactoimg1, cactoimg2, cactoimg3;
+let cactoimg4, cactoimg5, cactoimg6;
+let pontos = 0;
+let gameOver, restart;
+let gameOverImg, restartImg;
+let grupocactos, gruponuvens;
 
-var JOGANDO = 1;
-var PERDEU = 0;
-var estado = JOGANDO;
+let JOGANDO = 1;
+let PERDEU = 0;
+let estado = JOGANDO;
+
+let sompulo, sommorte, somcheckpoint;
 
 // Funçao para carregar os arquivos que vao ser usados
 function preload() {
@@ -28,6 +30,10 @@ function preload() {
 
   gameOverImg = loadImage("gameOver.png");
   restartImg = loadImage("restart.png");
+
+  sompulo = loadSound("jump.mp3");
+  sommorte = loadSound("die.mp3");
+  somcheckpoint = loadSound("checkpoint.mp3");
 }
 
 // Função que vai ser executada apenas uma vez
@@ -45,11 +51,13 @@ function setup(){
   trex.addAnimation("trex quando morreu", trexMorto);
   trex.scale = 0.5;
   trex.depth = 2;
+  // trex.debug = true;
+  trex.setCollider("circle",0,0,30);
 
   // cria e configura o chao
   chao = createSprite(300,175,600,10);
   chao.addImage("chao do jogo", chaoImagem);
-  chao.velocityX = -3;
+  chao.velocityX = -5;
 
   chaoInvisivel = createSprite(300,190,600,10);
   chaoInvisivel.visible = false;
@@ -64,16 +72,12 @@ function setup(){
   restart.addImage(restartImg);
   restart.scale = 0.5;
   restart.visible = false;
+
 }
 
 // Função que vai ser executada em loop
 function draw(){
   background("white");
-
-  // verifica se perdeu
-  if(trex.isTouching(grupocactos)){
-    estado = PERDEU;
-  }
   
   if(estado === JOGANDO){
     numAleat = Math.round(random(10,100));
@@ -86,7 +90,18 @@ function draw(){
 
     // trex colide com o chao (nao cai)
     trex.collide(chaoInvisivel);
-    
+
+    // verifica se perdeu
+    if(trex.isTouching(grupocactos)){
+      estado = PERDEU;
+      sommorte.play();
+    }
+
+    // verifica o checkpoint
+    if(pontos%1500 === 0 && pontos > 0){
+      somcheckpoint.play();
+    }
+
   }
 
   if(estado === PERDEU){
@@ -99,7 +114,15 @@ function draw(){
     gameOver.visible = true;
     restart.visible = true;
 
+    grupocactos.setLifetimeEach(-1);
+    gruponuvens.setLifetimeEach(-1);
+
+
     trex.changeAnimation("trex quando morreu");
+
+    if(mousePressedOver(restart) || keyDown("space")){
+      reiniciar();
+    }
 
 
   }
@@ -112,11 +135,12 @@ function pular() {
     //keyIsDown(UP_ARROW)
   if(keyDown("space") && trex.isTouching(chao) ){
     trex.velocityY = -13;
+    sompulo.play();
   }
   trex.velocityY += 0.8;  
 }
 
-// funcao que permite o chao ser ibfinito (nao acabar nunca)
+// funcao que permite o chao ser infinito (nao acabar nunca)
 function infinity() {
   if(chao.x < 0){
     chao.x = chao.width/2;
@@ -126,7 +150,7 @@ function infinity() {
 // funcao que gera as nuvens
 function criaNuvens() {
   if (frameCount%150 === 0|| frameCount === 1){
-    var nuvem = createSprite(610,numAleat,20,15);
+    let nuvem = createSprite(610,numAleat,20,15);
     nuvem.velocityX = -1;
     nuvem.addImage(nuvemImg);
     nuvem.scale = 0.7;
@@ -139,13 +163,13 @@ function criaNuvens() {
 // funcao que gera os cactos
 function criaObstaculos() {
   if (frameCount%80 === 0 || frameCount === 5){
-    var cacto = createSprite(610,160,20,15);
-    cacto.velocityX = -3;
+    let cacto = createSprite(610,160,20,15);
+    cacto.velocityX = -5;
     cacto.scale = 0.5;
     cacto.depth = 1;
     cacto.lifetime = 500;
     
-    var escolha = Math.round(random(1,6));
+    let escolha = Math.round(random(1,6));
     switch(escolha){
       case 1:
         cacto.addImage(cactoimg1);
@@ -176,5 +200,20 @@ function criaObstaculos() {
 // funcao que calcula e exibe a pontuação
 function pontuacao() {
   text("PONTUAÇÃO: " + pontos, 400,50);
-  pontos = pontos + Math.round(frameCount/60);
+  pontos = pontos + Math.round(getFrameRate()/55);
+}
+
+// funcao para reiniciar o jogo
+function reiniciar() {
+  estado = JOGANDO;
+  pontos = 0;
+  chao.velocityX = -5;
+  
+  grupocactos.destroyEach();
+  gruponuvens.destroyEach();
+
+  restart.visible = false;
+  gameOver.visible = false;
+
+  trex.changeAnimation("trex quando está correndo");
 }
